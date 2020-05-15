0aea53c4956c947b6e011e8189f8c4e97e6e0aae
05peaunt <enk125@mail.ru>
2020-05-15 22:28:22 +0500

    'wkhtmltopdf_port_32769'

--- checks/asynchronous_tasks.py
+++ checks/asynchronous_tasks.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django_rq import job
+#from django_rq import job
 
 
 import json
@@ -19,20 +19,22 @@ import base64
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 '''генерация html и pdf с информацией о заказе'''
-@job
+#@job
 def make_html_and_pdf(request, current_order_id, address, client_name, phone, goods, total, printer_check_type, check_id):
     context = {"current_order_id" : current_order_id, "address" : address, "client_name" : client_name, "phone" : phone, "goods" : goods, "total" : total}
     data = render_to_string(str(printer_check_type) +'_check.html', context)
     if not os.path.exists(BASE_DIR + '/media/html/' + str(current_order_id) + '_' + str(printer_check_type) + '.html'):
         with open((BASE_DIR + '/media/html/' + str(current_order_id) + '_' + str(printer_check_type) + '.html'), 'w') as f:
             f.write(data)
-    url = 'http://localhost:8005/'
+
+
+    url = 'http://localhost:32769/'
     contents =  open((BASE_DIR + '/media/html/' + str(current_order_id) + '_' + str(printer_check_type) + '.html'), 'rt').read()
     data = {'contents': contents}
     headers = {
         'Content-Type': 'application/json',
     }
-    response = requests.post(url, data=json.dumps(data).encode('utf-8'), headers=headers)
+    response = requests.post(url, data=json.dumps(data).encode('ascii'), headers=headers)
     with open(BASE_DIR + '/media/pdf/' + str(current_order_id) + '_' + str(printer_check_type) + '.pdf', 'wb') as f:
         f.write(response.content)
         f.close()
